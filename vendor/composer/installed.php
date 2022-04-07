7be5299effbbb4f53c5bd0d1693423a6f2dc5da0
svadhis <imeian@gmail.com>
2022-04-07 16:08:41 +0200

    composer added

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '0aee5379680539600c885181ad5c654e6bf05e3b',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '0aee5379680539600c885181ad5c654e6bf05e3b',
+            'dev_requirement' => false,
+        ),
+    ),
+);
